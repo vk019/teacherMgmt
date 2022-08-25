ff73d5e808e1e82db763a8e9a88a40aa832a919b
Mahima Sahu <mahi.sahu0711@gmail.com>
2022-08-25 22:29:02 +0530

    fgh
    changes done

---
+++ portal/src/component/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './css/home.css'
+
+function Home() {
+  return (
+    <>
+    <div>
+      <section id="home">
+        <div className="holder">
+          <div className="row justify-content-center">
+            <div className="col-md-8 mt-5">
+              <h1 className="display-4 fw-bolder mb-4 text-center">
+              The Origin of the Idea
+              </h1>
+              <p className="lead  text-center fs-4 mb-5">
+              Every year through the government exams plenty of teachers are hired and posted in various government schools.
+              The tracking of teachers from recruitment to their entire service duration is done manually which makes it difficult to manage and keep track.
+<br/>
+
+              The platform would be helpful in keeping track of all the government teachers after their CTET test. 
+              It would streamline the process of recruitment which is a very time-consuming, repetitive, and mundane task, 
+              which also includes a huge amount of manual errors and the time required to understand and resolve those errors. 
+              So, to eliminate unnecessary time waste and to make the process of keeping track of teachers' performance efficient and easy, 
+              we propose an idea of a common portal for gov. authorities, teachers, schools to provide a centralized digital platform, 
+              where all the three entities can communicate easily and effectively. 
+              In this way, the digitalization of the process can be implemented, 
+              which will free teachers and administrators from their burdensome work 
+              and allow them to spend more time focusing on their key goals.
+
+              </p>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    </>
+  )
+}
+
+export default Home
--- portal/src/component/Navbar.js
+++ portal/src/component/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
    <>
    <nav className="navbar navbar-expand-lg bg-primary">
   <div className="container-fluid">
-    <Link className="navbar-brand mx-3" to="/">Teacher Portal</Link>
+    <Link className="navbar-brand mx-3" to="/Home">Teacher Portal</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
