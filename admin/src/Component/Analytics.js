8245ba32f1edd26f9671816c6d3aaef5552114f1
vk019 <vidhit82@gmail.com>
2022-08-26 16:26:01 +0530

    last commit

--- admin/src/Component/Analytics.js
+++ admin/src/Component/Analytics.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 function Analytics() {
-  return (
-    <div><iframe title="Hello" width="1200px" height="650px" className="ml-4"src="https://sihvac2022.herokuapp.com/" /></div>
+  return (<>
+    <div className="row">
+      <div className="col">
+      <iframe title="Hello" width="1250px" height="700px" className="ml-2"src="https://sihvac2022.herokuapp.com/" />
+      </div>
+      </div>
+    <div className="row">
+    <div className="col">
+    <iframe title="Hello1" width="1250px" height="700px" className="ml-2"src="https://whispering-plains-94399.herokuapp.com/" />
+    </div>
+    </div>
+    </>
   )
 }
 
